e22d7ef9638a30e4b9f7e07de3b5aa6971118712
Theo-Matcharashvili <90640276+Theo-Matcharashvili@users.noreply.github.com>
2021-11-08 17:41:48 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const slides = document.querySelectorAll('.slide-item');
+const slidesLength = slides.length;
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
+const stopBtn = document.querySelector('#stop-sliding');
+const startBtn = document.querySelector('#start-sliding');
+let activeIndex = 0;
+
+function renderSlider() {
+  slides.forEach((element, index) => {
+    element.style.transform = `translateX(${100 * (index - activeIndex % slidesLength)}%)`;
+  })
+}
+
+renderSlider();
+
+function nextSlide() {
+  if(activeIndex === (slidesLength - 1)){
+    activeIndex = 0;
+  } else {
+    activeIndex = activeIndex + 1;
+  }
+
+  renderSlider();
+}
+
+function prevSlide() {
+  if(activeIndex === 0){
+    activeIndex = slidesLength - 1;
+  } else {
+    activeIndex = activeIndex - 1;
+  }
+
+  renderSlider();
+}
+
+nextButton.addEventListener('click', (e) => {
+  nextSlide();
+});
+prevButton.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.code);
+  if(e.code === 'ArrowRight'){
+    nextSlide();
+  }
+  if(e.code === 'ArrowLeft'){
+    prevSlide();
+  }
+});
+
+let intervalId = null;
+// task 2 mouseenter/mouseleave
+// I know it's not the best way but it works :D
+  document.getElementById("img1").onmouseenter = function() {startAutoSliding()};
+  document.getElementById("img1").onmouseleave = function() {stopAutoSliding()};
+  document.getElementById("img2").onmouseenter = function() {startAutoSliding()};
+  document.getElementById("img2").onmouseleave = function() {stopAutoSliding()};
+  document.getElementById("img3").onmouseenter = function() {startAutoSliding()};
+  document.getElementById("img3").onmouseleave = function() {stopAutoSliding()};
+  document.getElementById("img4").onmouseenter = function() {startAutoSliding()};
+  document.getElementById("img4").onmouseleave = function() {stopAutoSliding()};
+function startAutoSliding() {
+  if(!intervalId){
+    intervalId = setInterval(() => {
+      nextSlide();
+    }, 2000);
+  }
+}
+
+function stopAutoSliding() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+stopBtn.addEventListener('click', stopAutoSliding);
+startBtn.addEventListener('click', startAutoSliding);
+
+/* needs more work :(
+// button slide 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide-item");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  }
